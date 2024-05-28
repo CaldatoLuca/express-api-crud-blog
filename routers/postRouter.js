2c2401700e3e28653298dbcf7a2fb2995549d995
CaldatoLuca <lucacaldato15@gmail.com>
2024-05-28 17:46:39 +0200

    feat: applicare logica di controllo middleware anche su show

--- routers/postRouter.js
+++ routers/postRouter.js
@@ -7,7 +7,7 @@ const multer = require("multer");
 const postController = require("../controllers/postController");
 
 //Middlewares
-const checkDestroy = require("../middlewares/checkDestroy");
+const checkPostExist = require("../middlewares/checkPostExist");
 const uploader = multer({ dest: "public/imgs/posts" });
 
 //Pagina lista con metodo index
@@ -18,10 +18,10 @@ router.get("/", postController.index);
 router.post("/", uploader.single("image"), postController.store);
 
 //Dettaglio post con metodo show
-router.get("/:slug", postController.show);
+router.get("/:slug", checkPostExist, postController.show);
 
 //Dettaglio post con metodo show
-router.delete("/:slug", checkDestroy, postController.destroy);
+router.delete("/:slug", checkPostExist, postController.destroy);
 
 //Download con metodo download
 router.get("/:slug/download", postController.download);
